ca5895c627da12fcea9d14f0246a5f4da8cc6dca
LeMagicMan <104869812+LeMagicMan@users.noreply.github.com>
2024-05-02 19:33:10 +0200

    cleanup code and move GraphHistory.py to src/test/GraphHistory.py

--- src/test/GraphHistory.py
+++ src/test/GraphHistory.py
@@ -13,7 +13,7 @@ class GraphHistory:
         except (FileNotFoundError, EOFError):
             self.history = deque(maxlen=20)
 
-    def add_graph(self, graph, filterUsed):
+    def addGraph(self, graph, filterUsed):
         timestamp = time.time()
         self.inputCount += 1
         self.filterString = filterUsed
@@ -25,15 +25,16 @@ class GraphHistory:
             'graph': graph
         }
         self.history.append(graphData)
-        self.save_history()
+        self.saveHistory()
 
-    def save_history(self, pathName):
+    def saveHistory(self, pathName):
         with open(pathName, 'a') as f:
-            for i in range(0, len(self.history['recent graphs']), 20):
-                graphs = self.history['recent graphs'][i:i+20]
-                line = f"{time.time()}\t{len(self.history['input graphs'])}\t{len(self.history['output graphs'])}\t{self.filterString}\t{graphs}\n"
+            for i in range(0, len(self.history), 20):
+                graphs = self.history[i:i+20]
+                line = f"{time.time()}\t{len(self.history)}\t{len(self.history)}\t{self.filterString}\t{graphs}\n"
                 f.write(line)
 
-    def load_history(self):
+    def loadHistory(self):
         with open(self.pathName, 'rb') as f:
             self.history = pickle.load(f)
+
