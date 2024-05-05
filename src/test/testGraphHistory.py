eb1583a1d69273c548a56d7f844a9c36585ae68e
LeMagicMan <104869812+LeMagicMan@users.noreply.github.com>
2024-05-05 19:07:43 +0200

    bugfix testGraphHistory.py

--- src/test/testGraphHistory.py
+++ src/test/testGraphHistory.py
@@ -12,18 +12,21 @@ class TestGraphHistory(unittest.TestCase):
         self.assertEqual(len(self.gh.history), 1)
 
     def testSaveAndLoadHistory(self):
-        graph = nx.Graph()
-        self.gh.addGraph(graph, 'filter')
-        self.gh.loadHistory()
-        self.assertEqual(len(self.gh.history), 1)
+    graph = nx.Graph()
+    self.gh.addGraph(graph, 'filter')
+    self.gh.loadHistory()
+    self.gh.history.clear()
+    self.assertEqual(len(self.gh.history), 1)
 
-    def testSaveAndLoadMultipleGraphs(self):
-        graph1 = nx.Graph()
-        graph2 = nx.Graph()
-        self.gh.addGraph(graph1, 'filter1')
-        self.gh.addGraph(graph2, 'filter2')
-        self.gh.loadHistory()
-        self.assertEqual(len(self.gh.history), 2)
+def testSaveAndLoadMultipleGraphs(self):
+    graph1 = nx.Graph()
+    graph2 = nx.Graph()
+    self.gh.addGraph(graph1, 'filter1')
+    self.gh.addGraph(graph2, 'filter2')
+    self.gh.loadHistory()
+    self.gh.history.clear()
+    self.assertEqual(len(self.gh.history), 2)
+
         
     if __name__ == '__main__':
         unittest.main()
