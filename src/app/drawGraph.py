a668b3883a819a0884b9bd0a91d83f48ccc57db7
Mirovh <>
2024-05-07 15:16:34 +0200

    Moved drawGraph

---
+++ src/app/drawGraph.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def draw_graph(graph, path):
+    """Draws a graph and saves it to a file.
+
+    Args:
+        graph (nx.Graph): The graph to draw.
+        path (str): The path to the file where the graph will be saved.
+    """
+    nx.draw(graph)
+    plt.savefig(path)
